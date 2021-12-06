9e869f28fed440c2e1434fbf78987d6dd1391697
Alina <80460626+alishney@users.noreply.github.com>
2021-12-06 23:49:38 +0300

    изменила под себя

--- index.php
+++ index.php
@@ -4,44 +4,70 @@ include './questions.php'
 
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Тест</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-        label.form-check-label {
-            cursor: pointer;
-        }
+    label.form-check-label {
+        cursor: pointer;
+    }
 
-        body {
-            padding: 5rem 6rem;
-        }
+    body {
+        padding: 5rem 6rem;
+    }
+
+    p {
+        color: #2e30c7;
+    }
+
+    h1 {
+        text-align: center;
+        margin-top: -40px;
+        margin-bottom: 40px;
+        color: #2e30c7;
+    }
+
+    .button {
+        background-color: #2e30c7;
+        border-radius: 5rem;
+    }
     </style>
 </head>
 
 <body>
     <!--  Генерируем форму на основании массива questions  -->
+    <h1>Тест по инженерной графике</h1>
     <form action="results_page.php" method="POST">
         <?php foreach ($questions as $index => $question) { ?>
-            <div class="border p-4 rounded my-5 bg-light shadow-sm question-card user-select-none">
-                <p class="text-muted fw-light fs-5">Вопрос # <?= $index + 1 ?></p>
-                <?php if (isset($question['img'])) { ?>
-                    <img src="<?= $question['img'] ?>" class="border" style="max-width: 400px;" alt="изображение <?= $question['img'] ?> не загрузилось" />
-                <?php } ?>
-                <p class="mt-4 fw-bold fs-4"><?= $question['question'] ?></p>
+        <div>
+            <?php if (isset($question['img'])) { ?>
+            <img src="<?= $question['img'] ?>" class="border" style="max-width: 400px;"
+                 alt="изображение <?= $question['img'] ?> не загрузилось" />
+            <?php } ?>
+
+            <p class="mt-4 fw-bold fs-4">Вопрос №<?= $index + 1 ?> - <?= $question['question'] ?></p>
+            <div class="answers ps-5">
                 <?php foreach ($question['answers'] as $answer => $correct) { ?>
-                    <div class="form-check form-check-inline">
-                        <label class="form-check-label form-control-lg">
-                            <input class="form-check-input" type="radio" value="<?= $answer ?>" name="<?= $question['id'] ?>" checked>
-                            <?= $answer ?>
-                        </label>
-                    </div>
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label form-control-lg fs-5">
+                        <input class="form-check-input" type="radio" value="<?= $answer ?>"
+                               name="<?= $question['id'] ?>" checked>
+                        <?= $answer ?>
+                    </label>
+                </div>
                 <?php } ?>
             </div>
+            <hr>
+        </div>
         <?php } ?>
-        <button class="btn btn-primary" type="submit">Завершить тест</button>
+        <div style="text-align: center;">
+            <button class="btn btn-primary button px-5 py-2" type="submit">Отправить</button>
+        </div>
     </form>
 </body>
 
--- results_page.php
+++ results_page.php
@@ -1,27 +1,7 @@
 <?php
-/**
- * Данные с страницы теста приходят сюда
- * в массиве $_POST в формате массива
- * [
- *      "id вопроса" => "ответ который выбрал студент,
- *      "id вопроса" => "ответ который выбрал студент,
- *      "id вопроса" => "ответ который выбрал студент,
- *      ...
- * ]
- */
 
-/**
- * Подключаем исходные ответы что-бы
- * сравнивать ответ ученика с
- * правильным ответом
- */
 include './questions.php';
 
-
-/**
- * Функция которая принимает в себя массив
- * с вопросами, и ищет в нем вопрос по его id
- */
 function findQuestionById(string $questionId, array $questions)
 {
     foreach ($questions as $question) {
@@ -31,56 +11,39 @@ function findQuestionById(string $questionId, array $questions)
     }
 }
 
-/**
- * Всего правильных ответов которые набрал ученик
- */
 $totalPoints = 0;
 
-/**
- * В эту переменную будут добавляться
- * ряды с данными для html таблицы
- * сделал это что бы сгенерировать html отдельно
- * и не устраивать кашу php и html
- */
-$htmlTableRows = '';
-
-/**
- * Проходим массив данных POST и
- */
 foreach ($_POST as $questionId => $studentAnswer) {
-    /** Ищем вопрос по его id */
+ 
     $question = findQuestionById($questionId, $questions);
-    /** получаем корректный ответ на этот вопрос */
+   
     $correctAnswer = array_search(true, $question['answers']);
-    /**
-     * сравниваем ответ студента и правильный ответ из исходных днных,
-     * и результат этого сравнения заносим в эту переменную
-     */
+    
     $studentAnswerIsCorrect = $correctAnswer == $studentAnswer;
 
-    /** добавляем очки если ответ студента совпадает с правильным */
     if ($studentAnswerIsCorrect) {
         $totalPoints++;
     }
 
-    /**
-     * добавляем к рядам таблицы еще один, это по сути строка,
-     * называется HEREDOC, этот синтаксис используется когда надо большие строки делать
-     * по итогу мы просто добавляем к переменной еще одну строку в которой html ряд таблицы
-     * просто строка обычная, тип string
-     */
-    $htmlTableRows .= <<<ROW
-                    <tr>
-                        <td>{$question['question']}</td>
-                        <td>{$correctAnswer}</td>
-                        <td>{$studentAnswer}</td>
-                    <tr/>
-                ROW;
+   
 }
 
-/** дополнительно посчитал проценты, нет необходдимости, для красоты */
-$percents = ($totalPoints / count($questions)) * 100 . '%';
-?>
+$mark = 0;
+
+$percents = round(($totalPoints / count($questions)) * 100) . '%';
+
+ if ($percents < 50){
+    $mark = 2;
+ }
+ else if  ($percents < 71){
+$mark = 3;
+ }
+  else if  ($percents < 91){
+ $mark = 4;
+  }
+ 
+  else {$mark = 5;}
+  ?>
 
 
 <!DOCTYPE html>
@@ -94,37 +57,20 @@ $percents = ($totalPoints / count($questions)) * 100 . '%';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
-        table tr > *:not(:first-child) {
-            white-space: nowrap;
-        }
+    table tr>*:not(:first-child) {
+        white-space: nowrap;
+    }
     </style>
 </head>
 
 <body class="p-5">
-<div class="container">
-    <!--  Во всей этой таблице нет необходимости, её можно убрать и вместо нее просто вывести $totalPoints, или как-то по своему  -->
-    <!-- ======================================================================================================================== -->
-    <table class="table table-bordered text-secondary">
-        <tbody>
-        <tr>
-            <th>Вопрос</th>
-            <th>Правильный ответ</th>
-            <th>Ответ ученика</th>
-        </tr>
-        <!-- Выводим сюда ряды таблицы которые в php собирали -->
-        <?= $htmlTableRows ?>
-        <tr class="fw-bold">
-            <td colspan="2">Итог</td>
-            <td><?= $totalPoints ?> / <?= count($questions) ?> вопросов (<?= $percents ?>)</td>
-        </tr>
-        </tbody>
-    </table>
-    <div class="p-4">
-        <p class="text-muted"><small>* примите во внимание что не на всех устройствах эта функция работает точно, и даже
-                случайный клик за пределы области браузера может привести к её срабатыванию</small></p>
+    <div class="container">
+
+        <p style="font-size: 40px;" colspan="2">Результат:</p>
+        <p style="font-size: 20px;"><?= $totalPoints ?> из <?= count($questions) ?> вопросов (<?= $percents ?>)</p>
+        <p style="font-size: 20px;">Оценка: <?=$mark?></p>
+
     </div>
-    <!-- ======================================================================================================================== -->
-</div>
 </body>
 
 </html>
